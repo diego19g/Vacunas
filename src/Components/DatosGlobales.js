bee2343395ea3519435f4c78f80826618461918d
diego19g <diego19rm@gmail.com>
2021-02-23 17:51:45 +0100

    cambios

--- src/Components/DatosGlobales.js
+++ src/Components/DatosGlobales.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import "./estilos.css";
 
 function DatosGlobales() {
     const baseUrl = "http://localhost:4004/vacunas";
